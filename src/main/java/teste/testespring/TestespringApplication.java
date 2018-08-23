588c5f468c696333eccb3724a2c6669ff8ae47b1
Carlos César <carlosmorais10@hotmail.com>
2018-08-23 17:29:08 -0300

    Projeto criado

---
+++ src/main/java/teste/testespring/TestespringApplication.java
@@ -0,0 +1,12 @@
+package teste.testespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestespringApplication.class, args);
+	}
+}
