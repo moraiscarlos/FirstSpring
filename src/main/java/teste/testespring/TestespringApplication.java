b146079930f67a9aa798bee499972ac4b801da61
Carlos César <carlosmorais10@hotmail.com>
2018-08-24 14:29:53 -0300

    Criando operação de instanciação

--- src/main/java/teste/testespring/TestespringApplication.java
+++ src/main/java/teste/testespring/TestespringApplication.java
@@ -1,12 +1,31 @@
 package teste.testespring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import teste.testespring.domain.Categoria;
+import teste.testespring.repositories.CategoriaRepository;
+
+import java.util.Arrays;
 
 @SpringBootApplication
-public class TestespringApplication {
+public class TestespringApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestespringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 =  new Categoria(null, "Informática");
+		Categoria cat2 =  new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 }
